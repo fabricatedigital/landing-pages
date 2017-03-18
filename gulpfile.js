22fb3b82fe9d683f510a1397d2bdd27e8a075daa
Jordan Worner <jordan@fabricatedigital.com>
2017-03-18 13:27:59 +0000

    set up sass and ejs tasks. added assets for fabricatejs

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var ejs = require('gulp-ejs');
+var sass = require('gulp-sass');
+
+gulp.task('template:fj', function() {
+  gulp.src('./index.ejs')
+    .pipe(ejs({
+        title: 'FabricateJS',
+        description: 'node.js framework',
+        ga: '',
+        email: 'info@fabricatejs.com'
+    }, {}, { ext: '.html' }))
+    .pipe(gulp.dest('./fabricatejs.com'))
+});
+
+gulp.task('sass:fj', function (){
+  return gulp.src('./scss/fabricatejs/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./fabricatejs.com/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./scss/**/*.scss'], ['sass:fj']);
+  gulp.watch(['./index.ejs'], ['template:fj']);
+});
