c8007a5a8787aab458a82df970624d7baa546f32
Jordan Worner <jordan@fabricatedigital.com>
2017-03-18 18:49:29 +0000

    add savantlms.com

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,17 @@ gulp.task('template:fj', function() {
     .pipe(gulp.dest('./fabricatejs.com'))
 });
 
+gulp.task('template:sl', function() {
+  gulp.src('./index.ejs')
+    .pipe(ejs({
+        title: 'Savant LMS',
+        description: 'learning management system',
+        ga: 'UA-93839334-4',
+        email: 'info@savantlms.com'
+    }, {}, { ext: '.html' }))
+    .pipe(gulp.dest('./savantlms.com'))
+});
+
 gulp.task('sass:fj', function (){
   return gulp.src('./scss/fabricatejs/*.scss')
     .pipe(sass({
@@ -20,6 +31,16 @@ gulp.task('sass:fj', function (){
     }).on('error', sass.logError))
     .pipe(gulp.dest('./fabricatejs.com/css'));
 });
+
+gulp.task('sass:sl', function (){
+  return gulp.src('./scss/savantlms/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./savantlms.com/css'));
+});
+
+gulp.task('build', ['template:fj', 'template:sl', 'sass:fj', 'sass:sl']);
 
 gulp.task('watch', function() {
   gulp.watch(['./scss/**/*.scss'], ['sass:fj']);
